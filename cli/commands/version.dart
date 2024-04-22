b698e7171d447af3cf40e8351edc5b410834b743
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-04-22 22:55:46 +0600

    feat(cli): use version command to directly update version of pubspec and changelog generation

--- cli/commands/version.dart
+++ cli/commands/version.dart
@@ -4,8 +4,8 @@ import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:path/path.dart';
+import 'package:process_run/process_run.dart';
 import 'package:pub_semver/pub_semver.dart';
-import 'package:pubspec/pubspec.dart';
 import 'package:toml/toml.dart';
 
 class VersionCommand extends Command {
@@ -25,8 +25,9 @@ class VersionCommand extends Command {
     );
 
     argParser.addFlag(
-      'pubspec',
-      help: 'Update the version from the pubspec.yaml file.',
+      'no-tag-commit',
+      help: 'Do not create a git tag and commit the changes.',
+      negatable: false,
       defaultsTo: false,
     );
   }
@@ -49,33 +50,28 @@ class VersionCommand extends Command {
       return;
     }
 
-    final isPubspec = argResults?.flag("pubspec") == true;
-
-    if (argResults == null || (!isPubspec && argResults!.arguments.isEmpty)) {
+    if (argResults == null || argResults!.arguments.isEmpty) {
       stderr.writeln("Please provide a new <version>");
       return;
     }
 
     final newVersion = await Future.microtask(() async {
-      if (isPubspec) {
-        final pubspecFile = File(join(projectDir, "pubspec.yaml"));
-
-        if (!await pubspecFile.exists()) {
-          stderr.writeln("pubspec.yaml not found");
-          exit(1);
-        }
-
-        final pubspecContent = await pubspecFile.readAsString();
-        final pubspec = PubSpec.fromYamlString(pubspecContent);
-
-        return pubspec.version!;
-      }
-
       return Version.parse(argResults!.arguments.first);
     }).catchError((e) {
       stderr.writeln("Invalid version format");
       exit(1);
     });
+
+    final shell = Shell(workingDirectory: cwd);
+
+    final processResult = await shell.run("""
+      melos version -V $project:$newVersion --no-git-tag-version --no-git-commit-version --yes
+    """);
+
+    // exit if the process failed
+    if (processResult.first.exitCode != 0) {
+      exit(processResult.first.exitCode);
+    }
 
     final buildFiles = [
       join(projectDir, "ios", "$project.podspec"),
@@ -109,6 +105,16 @@ class VersionCommand extends Command {
       await cargoToml.writeAsString(
         TomlDocument.fromMap(content).toString(),
       );
+    }
+
+    if (argResults?.flag("no-tag-commit") == false) {
+      stdout.writeln("Version updated to $newVersion");
+      stdout.writeln("Committing and tagging changes...");
+      await shell.run("""
+      git add .
+      git commit -m "chore($project): bump version to v$newVersion"
+      git tag -a $project-v$newVersion -m "chore($project): release v$newVersion"
+    """);
     }
   }
 }
