ea12cdf4e343c1850eb47eb366a34a623b5a3b7a
Kingkor Roy Tirtho <krtirtho@gmail.com>
2024-07-14 16:48:18 +0600

    feat(flutter_discord_rpc): add auto retry connect

--- packages/flutter_discord_rpc/lib/src/flutter_discord_rpc_base.dart
+++ packages/flutter_discord_rpc/lib/src/flutter_discord_rpc_base.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_discord_rpc/src/ffi.dart';
 import 'package:flutter_discord_rpc/src/bridge_generated.dart' as bridge;
 
@@ -6,7 +8,11 @@ final class FlutterDiscordRPC {
   static FlutterDiscordRPC get instance => _instance!;
 
   final bridge.FlutterDiscordRpc _lib;
-  FlutterDiscordRPC._() : _lib = createLib();
+  final StreamController<bool> _isConnectedStreamController;
+
+  FlutterDiscordRPC._()
+      : _lib = createLib(),
+        _isConnectedStreamController = StreamController<bool>.broadcast();
 
   static Future<void> initialize(String applicationId) async {
     if (_instance != null) {
@@ -19,13 +25,37 @@ final class FlutterDiscordRPC {
 
   bool _isConnected = false;
   bool get isConnected => _isConnected;
+  Stream<bool> get isConnectedStream => _isConnectedStreamController.stream;
 
-  Future<void> connect() async {
+  Timer? _timer;
+
+  void _onConnected(bool connected) {
+    _isConnected = connected;
+    _isConnectedStreamController.add(connected);
+  }
+
+  Future<void> connect({
+    bool autoRetry = false,
+    Duration retryDelay = const Duration(seconds: 5),
+  }) async {
     try {
       await _lib.discordConnect();
-      _isConnected = true;
+      _onConnected(true);
+      _timer?.cancel();
     } catch (e) {
-      _isConnected = false;
+      _onConnected(false);
+      if (autoRetry) {
+        _timer = Timer.periodic(retryDelay, (timer) async {
+          try {
+            await _lib.discordConnect();
+            _onConnected(true);
+            timer.cancel();
+          } catch (e) {
+            _onConnected(false);
+          }
+        });
+        return;
+      }
       rethrow;
     }
   }
@@ -33,9 +63,10 @@ final class FlutterDiscordRPC {
   Future<void> disconnect() async {
     try {
       await _lib.discordClose();
-    } catch (e) {
-      _isConnected = false;
-      rethrow;
+    } finally {
+      _timer?.cancel();
+      _timer = null;
+      _onConnected(false);
     }
   }
 
@@ -55,6 +86,8 @@ final class FlutterDiscordRPC {
   }
 
   Future<void> dispose() async {
+    _timer?.cancel();
+    _timer = null;
     await _lib.discordDispose();
   }
 }
